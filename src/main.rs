c408146efdbb646acb25daba65879bfbdc3249ff
Rohan Ankarigari Boda <23293949+Rahtoken@users.noreply.github.com>
2020-06-07 16:43:40 +0000

    Fix max length bug and add integration test cases for max length.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     let length: i32 = get_cli_param_or_default(&cli_args, "length").unwrap();
     let password_length = match length {
         l if l < 0 => DEFAULT_LENGTH,
-        l if l > max_length && max_length > 16 => DEFAULT_MAX_LENGTH,
+        l if l > max_length => DEFAULT_MAX_LENGTH,
         _ => length,
     };
     let password = gen_pwd(password_length as usize);
--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -17,4 +17,22 @@ mod cli_test {
             .satisfies(|p| p.chars().count() == 24, "Custom invocation successful")
             .unwrap();
     }
+
+    #[test]
+    fn max_length_override_test() {
+        assert_cli::Assert::main_binary()
+            .with_args(&["-l", "420", "-m", "1024"])
+            .stdout()
+            .satisfies(|p| p.chars().count() == 420, "Max length overriden")
+            .unwrap();
+    }
+
+    #[test]
+    fn invocation_defaults_to_max_length() {
+        assert_cli::Assert::main_binary()
+            .with_args(&["-l", "420"])
+            .stdout()
+            .satisfies(|p| p.chars().count() == 128, "Default max length")
+            .unwrap();
+    }
 }
