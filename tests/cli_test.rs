b537952b2022227c5085b6b3194c3a1fdf4467f1
Rohan Ankarigari Boda <23293949+Rahtoken@users.noreply.github.com>
2020-06-07 10:34:33 +0000

    Improve integration tests setup.

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -1,16 +1,20 @@
-use std::process::Command;
+extern crate assert_cli;
 
 mod cli_test {
     #[test]
-    fn proper_invocation() {
-        let output = std::process::Command::new("./target/release/pgen")
-            .output()
-            .expect("Failed to invoke process");
-        if let Ok(pwd) = String::from_utf8(output.stdout) {
-            println!("{:#?}", (&pwd, pwd.chars().count()));
-            assert_eq!(pwd.chars().count(), 16);
-        } else {
-            panic!("Invalid output generated!");
-        };        
+    fn default_invocation() {
+        assert_cli::Assert::main_binary()
+            .stdout()
+            .satisfies(|p| p.chars().count() == 16, "Invocation successful")
+            .unwrap();
+    }
+
+    #[test]
+    fn successful_invocation_with_custom_length() {
+        assert_cli::Assert::main_binary()
+            .with_args(&["-l", "24"])
+            .stdout()
+            .satisfies(|p| p.chars().count() == 24, "Custom invocation successful")
+            .unwrap();
     }
 }
